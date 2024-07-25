32f434ae0a2ceeac30bded25509953a0c3b41c83
GIta Asaria <yosephine.gah@gmail.com>
2024-07-26 00:02:55 +0800

    first commit

---
+++ src/main.js
@@ -0,0 +1,20 @@
+const globalContainer = document.querySelector(".container").style;
+globalContainer.color="black";
+globalContainer.textAlign = "center";
+
+document.querySelector("button").style;
+
+const randomRecipe = {
+    title:"Spaghetti Carbonara", 
+    instructions: "1. Cook spaghetti according to package instructions. 2. In a separate pan, cook pancetta until crispy. 3. In a bowl, whisk eggs and grated cheese. 4. Combine spaghetti, pancetta, and egg mixture, stirring quickly. 5. Serve immediately with additional cheese and black pepper.", 
+    image: "https://www.themealdb.com/images/media/meals/llcbn01574260722.jpg",
+}
+
+document.getElementById("getRecipeBtn").addEventListener("click",fetchData);
+
+
+function fetchData(){
+document.getElementsById("recipe-title").innerHTML=randomRecipe.title;
+document.getElementById("recipe-image").src=randomRecipe.image;
+document.getElementById("recipe-instruction").innerHTML=randomRecipe.instructions;
+}
